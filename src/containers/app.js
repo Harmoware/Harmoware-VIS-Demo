19cb757d6628152bdb83fee5b04acea41570bf8b
Yamauchi Yoshitaka <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2022-06-13 10:52:01 +0900

    update

--- src/containers/app.js
+++ src/containers/app.js
@@ -3,41 +3,39 @@ import {
   Container, connectToHarmowareVis, HarmoVisLayers, MovesLayer, MovesInput
 } from 'harmoware-vis';
 
-const MAPBOX_TOKEN = 'xxxxxxxxxx'; //Acquire Mapbox accesstoken
+const MAPBOX_TOKEN = 'pk.eyJ1IjoieW11Y3lzdGsiLCJhIjoiY2oxdmhhbmd0MDAwYjM4bXd1YWVodWNrcCJ9.aWxoDc0UXMVGB96b82GFKQ'; //Acquire Mapbox accesstoken
 
-class App extends Container {
-  render() {
-    const { actions, clickedObject, inputFileName, viewport,
-      routePaths, movesbase, movedData } = this.props;
-    const { movesFileName } = inputFileName;
-    const optionVisible = false;
+const App = (props)=>{
+  const { actions, clickedObject, inputFileName, viewport,
+    routePaths, movesbase, movedData } = props;
+  const { movesFileName } = inputFileName;
+  const optionVisible = false;
 
-    return (
-      <div>
-        <div className="harmovis_controller">
-          <ul className="flex_list">
-            <li className="flex_row">
-              <div className="harmovis_input_button_column">
-              <label htmlFor="MovesInput">
-                Operation data<MovesInput actions={actions} id="MovesInput" />
-              </label>
-              <div>{movesFileName}</div>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className="harmovis_area">
-          <HarmoVisLayers
-            viewport={viewport} actions={actions}
-            mapboxApiAccessToken={MAPBOX_TOKEN}
-            layers={[
-              new MovesLayer({ routePaths, movesbase, movedData,
-                clickedObject, actions, optionVisible }),
-            ]}
-          />
-        </div>
+  return (
+    <Container {...props}>
+      <div className="harmovis_controller">
+        <ul className="flex_list">
+          <li className="flex_row">
+            <div className="harmovis_input_button_column">
+            <label htmlFor="MovesInput">
+              Operation data<MovesInput actions={actions} id="MovesInput" />
+            </label>
+            <div>{movesFileName}</div>
+            </div>
+          </li>
+        </ul>
       </div>
-    );
-  }
+      <div className="harmovis_area">
+        <HarmoVisLayers
+          viewport={viewport} actions={actions}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          layers={[
+            new MovesLayer({ routePaths, movesbase, movedData,
+              clickedObject, actions, optionVisible }),
+          ]}
+        />
+      </div>
+    </Container>
+  );
 }
 export default connectToHarmowareVis(App);
